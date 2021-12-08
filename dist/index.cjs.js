52f66bd0302bd3810333e171e62b66676ae55bb0
blockchainprofessor <blockchainprofessor@protonmail.com>
2021-12-08 15:09:04 +0700

    bodf11

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2868,7 +2868,7 @@ var baseColors = {
     primary: "#f48337",
     primaryBright: "#f48337",
     primaryDark: "#FFFFFF",
-    secondary: "#37733f",
+    secondary: "#313D5C",
     success: "#31D0AA",
     warning: "#FFB237",
 };
@@ -2878,7 +2878,7 @@ var brandColors = {
 var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#273b4f", textDisabled: "#BDC2C4", textSubtle: "#273b4f", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
     bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
 } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#51c4d3", background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#191326", input: "#eeeaf4", primaryDark: "#FFFFFF", tertiary: "#353547", text: "#273b4f", textDisabled: "#BDC2C4", textSubtle: "#273b4f", borderColor: "#524B63", card: "#FFFFFF", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#E9EAEB", background: "#191326", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#191326", input: "#eeeaf4", primaryDark: "#FFFFFF", tertiary: "#353547", text: "#273b4f", textDisabled: "#BDC2C4", textSubtle: "#273b4f", borderColor: "#524B63", card: "#FFFFFF", gradients: {
     bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
 } });
 
