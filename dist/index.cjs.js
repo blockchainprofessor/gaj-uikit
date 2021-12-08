bae3b1ec73a42fcc48b45730527fced610e87704
blockchainprofessor <blockchainprofessor@protonmail.com>
2021-12-08 14:47:42 +0700

    iu

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2864,21 +2864,21 @@ var ResetCSS = styled.createGlobalStyle(templateObject_1$K || (templateObject_1$
 var templateObject_1$K;
 
 var baseColors = {
-    failure: "#7F2D00",
-    primary: "#814fc1",
-    primaryBright: "#9477C3",
-    primaryDark: "#003ECF",
-    secondary: "#814fc1",
-    success: "#814FC1",
-    warning: "#8E4B5D",
+    failure: "#ED4B9E",
+    primary: "#0a0a09",
+    primaryBright: "#030303",
+    primaryDark: "#030303",
+    secondary: "#7e0101",
+    success: "#31D0AA",
+    warning: "#FFB237",
 };
 var brandColors = {
     binance: "#F0B90B",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#f8f0ff", backgroundDisabled: "#e9eaeb", contrast: "#191326", invertedContrast: "#f2f2f2", input: "#eeeaf4", tertiary: "#d5dcfa", text: "#12205c", textDisabled: "#8196f0", textSubtle: "#814FC1", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#800000", textDisabled: "#BDC2C4", textSubtle: "#970202", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
     bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
 } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#5773eb", background: "#00061D", backgroundDisabled: "#00061D", contrast: "#FFFFFF", invertedContrast: "#2948cf", input: "#eeeaf4", primaryDark: "#003ECF", tertiary: "#141826", text: "#ffffff", textDisabled: "#5773eb", textSubtle: "#abb9f5", borderColor: "#09102e", card: "#131725", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#9A6AFF", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0098A1", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#524B63", card: "#27262c", gradients: {
     bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
 } });
 
